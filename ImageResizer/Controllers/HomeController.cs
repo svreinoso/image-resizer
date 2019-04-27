2c6e52c995ce6b48e483efeeacc9c95a04b4402f
Samuel Reinoso <samuel.r.l@hotmail.com>
2019-04-27 17:42:56 -0400

    Initial commit

---
+++ ImageResizer/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using ImageResizer.Helpsers;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ImageResizer.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IImageHelper _imageHelper;
+        private readonly string _imageFolder = "~/Images/";
+        private readonly string _imageExtention = ".png";
+
+        public HomeController(IImageHelper imageHelper)
+        {
+            _imageHelper = imageHelper;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult UploadImage(HttpPostedFileBase file)
+        {
+            if (file != null && _imageHelper.IsImage(file.FileName))
+            {
+                var sizes = new List<int> { 16, 32, 64, 128, 256, 512, 1024 };
+                if (!Directory.Exists(Server.MapPath(_imageFolder))) Directory.CreateDirectory(Server.MapPath(_imageFolder));
+
+                var originalImageName = $"image-original{_imageExtention}";
+                string imageName = Path.GetFileName(originalImageName);
+                string path = Path.Combine(Server.MapPath(_imageFolder), imageName);
+                file.SaveAs(path);
+                ViewBag.originalImageName = $"/Images/{originalImageName}";
+
+                var image = Image.FromStream(file.InputStream, true, true);
+
+                sizes.ForEach(x => Resize(image, x, x));
+
+                ViewBag.sizes = sizes;
+
+                ViewBag.hasImage = true;
+                return View("Index");
+            }
+
+            ViewBag.errorMessage = "File is not an image.";
+            return View("Index");
+        }
+
+        private void Resize(Image image, int width, int height)
+        {
+            var imageResized50x50 = _imageHelper.ResizeImage(image, width, height);
+            var imageName = $"resizedImageName{width}x{height}";
+            var imageResizedPath = Path.Combine(Server.MapPath(_imageFolder), imageName + _imageExtention);
+            imageResized50x50.Save(imageResizedPath);
+            ViewData[imageName] = $"/Images/{imageName}{_imageExtention}";
+        }
+    }
+}
---
+++ ImageResizer/Helpers/ImageHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+namespace ImageResizer.Helpsers
+{
+    public interface IImageHelper
+    {
+        Image ResizeImage(Image image, int maxWidth, int maxHeight);
+        bool IsImage(string imageName);
+    }
+
+    public class ImageHelper : IImageHelper
+    {
+        public Image ResizeImage(Image image, int maxWidth, int maxHeight)
+        {
+            var width = (double)maxWidth / image.Width;
+            var height = (double)maxHeight / image.Height;
+            var ratio = Math.Min(width, height);
+        
+            var newWidth = (int)(image.Width * ratio);
+            var newHeight = (int)(image.Height * ratio);
+        
+            var bitMap = new Bitmap(newWidth, newHeight);
+            var rectangle = new Rectangle(0, 0, bitMap.Width, bitMap.Height);
+            var graphics = Graphics.FromImage(bitMap);
+            graphics.DrawImage(image, rectangle);
+        
+            return bitMap;
+        }
+
+        public bool IsImage(string imageName)
+        {
+            var validExtensions = new List<string> { "jpg", "gif", "png", "jpeg" };
+            return validExtensions.Any(x => imageName.ToLower().EndsWith(x.ToLower()));
+        }
+    }
+}
